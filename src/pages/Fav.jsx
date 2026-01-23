4eb37073c46b221dccab25456e4576253852cdc3
Nikhil Singhal <nikhilagarwal6305@gmail.com>
2026-01-23 22:39:02 +0530

    commit#6

--- src/pages/Fav.jsx
+++ src/pages/Fav.jsx
@@ -1,17 +1,21 @@
 import RecipesCard from "../components/RecipesCard";
 
 const Fav = () => {
+  const favourite = JSON.parse(localStorage.getItem("fav") || "[]");
 
-  const favourite = JSON.parse(localStorage.getItem("fav") || []);
-
-  const renderrecipe = favourite.map((recipe) => {
-    return <RecipesCard key={recipe.id} recipe={recipe} />
-  })
   return (
     <div className="flex justify-evenly mt-8 flex-wrap gap-12">
-      {favourite.length > 0 ? renderrecipe: <p className="text-4xl text-center opacity-30">No favorite found!</p>}
+      {favourite.length > 0 ? (
+        favourite.map((recipe) => (
+          <RecipesCard key={recipe.id} recipe={recipe} />
+        ))
+      ) : (
+        <p className="text-4xl text-center opacity-30">
+          No favorite found!
+        </p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Fav
+export default Fav;
